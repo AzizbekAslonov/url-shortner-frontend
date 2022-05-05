fa64a1280385e725756da3eac47557d84d96cf7b
Azizbek <aslonovazizbek318@gmail.com>
2022-05-05 07:32:55 +0500

    API_URL

--- src/index.js
+++ src/index.js
@@ -4,8 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-// export const API_URL = 'https://urla.herokuapp.com'
-export const API_URL = 'http://localhost:5000'
+export const API_URL = 'https://urla.herokuapp.com'
 
 ReactDOM.render(
     <React.StrictMode>
